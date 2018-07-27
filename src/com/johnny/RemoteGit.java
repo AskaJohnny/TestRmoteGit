53110a311cecf81bfbae05a20b790cf3af579f08
AskaJohnny <34227977+AskaJohnny@users.noreply.github.com>
2018-07-27 12:41:44 +0800

    Update RemoteGit.java

--- src/com/johnny/RemoteGit.java
+++ src/com/johnny/RemoteGit.java
@@ -17,8 +17,8 @@ public class RemoteGit {
     }
 
 
-    public void hello2(){
-        System.out.println("hello github");
+    public void hello(){
+        System.out.println("hello github   111");
     }
 
 
