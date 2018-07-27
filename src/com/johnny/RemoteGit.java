beccefab1d137ee715e0cf2b0a9b7a956a55c082
johnny <626142589@qq.com>
2018-07-27 11:01:47 +0800

    测试 远程提价到github上面

---
+++ src/com/johnny/RemoteGit.java
@@ -0,0 +1,15 @@
+package com.johnny;
+
+/**
+ * @author johnny
+ * @create 2018-07-27 上午10:59
+ **/
+public class RemoteGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello johnny  git");
+    }
+
+
+}
